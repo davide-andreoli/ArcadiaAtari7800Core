2b3194d9945bd8cc5f0baf8c78fe07040abd8a71
Davide Andreoli <davide.andreoli@icloud.com>
2024-07-18 01:06:11 +0200

    Initial Commit

---
+++ ArcadiaAtari7800Core/ArcadiaAtari7800Core.h
@@ -0,0 +1,18 @@
+//
+//  ArcadiaAtari7800Core.h
+//  ArcadiaAtari7800Core
+//
+//  Created by Davide Andreoli on 18/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArcadiaAtari7800Core.
+FOUNDATION_EXPORT double ArcadiaAtari7800CoreVersionNumber;
+
+//! Project version string for ArcadiaAtari7800Core.
+FOUNDATION_EXPORT const unsigned char ArcadiaAtari7800CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcadiaAtari7800Core/PublicHeader.h>
+
+
